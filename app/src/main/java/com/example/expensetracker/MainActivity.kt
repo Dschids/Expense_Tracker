6d621093c0c037f53cd7d48778d4b9c402f9e1a8
Dschids <123191756+Dschids@users.noreply.github.com>
2024-04-23 12:52:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/expensetracker/MainActivity.kt
@@ -0,0 +1,119 @@
+package com.example.expensetracker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.expensetracker.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    /* initilize shared preferences out here so it can be used in several functions without having
+    * to re-initialize each time
+    */
+
+    private lateinit var _main_binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _main_binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_main_binding.root)
+
+        var SP = getSharedPreferences(getString(R.string.my_set_file), MODE_PRIVATE)
+        var myEdit = SP.edit()
+
+//        myEdit.apply {
+//            putString("username", "")
+//            putString("password", "")
+//            commit()
+//        }
+
+
+        val toExpensesMain = Intent(this@MainActivity, ExpensesMain::class.java)
+
+        _main_binding.btnLogin.setOnClickListener {
+
+            var enteredName = _main_binding.etUserName.text.toString().lowercase()
+            var enteredPass = _main_binding.etPassword.text.toString()
+
+            // check if username and password exist
+            if (passwordExists()){
+                if (checkUserNameUserPass(enteredName, enteredPass)){
+
+                    startActivity(toExpensesMain)
+                }
+            } else {
+                if (fieldsFilled(enteredName, enteredPass)){
+                    myEdit.apply{
+                        putString("username", _main_binding.etUserName.text.toString().lowercase())
+                        putString("password", _main_binding.etPassword.text.toString())
+                        commit()
+                    }
+                    startActivity(toExpensesMain)
+                }
+
+            }
+        }
+    }
+
+    // checks whether a username and password are saved in shared preferences
+    private fun passwordExists(): Boolean {
+        var SP = getSharedPreferences(getString(R.string.my_set_file), MODE_PRIVATE)
+        val userName = SP.getString("username", "")
+        val userPass = SP.getString("password", "")
+
+        return !(userName!!.isEmpty() && userPass!!.isEmpty())
+    }
+
+    /* check whether the entered username and password match the saved user name and password
+    and return a boolean
+     */
+    private fun checkUserNameUserPass(enteredName: String, enteredPass: String): Boolean{
+        var SP = getSharedPreferences(getString(R.string.my_set_file), MODE_PRIVATE)
+        val userName = SP.getString("username", "")
+        val userPass = SP.getString("password", "")
+        var nameMatch = false
+        var passMatch = false
+
+        if (enteredName == userName){
+            nameMatch = true
+        }else {
+            if (enteredName.isEmpty()){
+                Toast.makeText(this, "Please enter a user name", Toast.LENGTH_SHORT).show()
+            }else {
+                Toast.makeText(this, "Unknown user name", Toast.LENGTH_SHORT).show()
+            }
+        }
+        if (enteredPass == userPass){
+            passMatch = true
+        }else {
+            if (enteredPass.isEmpty()){
+                Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT).show()
+            }else {
+                Toast.makeText(this, "Incorrect password", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return (nameMatch && passMatch)
+    }
+
+
+    // check if username and password have anything in them and returns a boolean
+    private fun fieldsFilled(enteredName: String, enteredPass: String): Boolean {
+        var nameFilled = false
+        var passFilled = false
+
+        if (enteredName.isEmpty()){
+            Toast.makeText(this, "Please enter a user name", Toast.LENGTH_SHORT).show()
+        }else {
+            nameFilled = true
+        }
+        if (enteredPass.isEmpty()){
+            Toast.makeText(this, "Please enter a password", Toast.LENGTH_SHORT).show()
+        }else {
+            passFilled = true
+        }
+
+        return (nameFilled && passFilled)
+    }
+}
